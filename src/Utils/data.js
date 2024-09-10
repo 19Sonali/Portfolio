7788dc359eaae10f85b11ace18e7fec3a4ad5481
19Sonali <109802850+19Sonali@users.noreply.github.com>
2024-09-10 13:30:03 +0530

    Updated work experience

--- src/Utils/data.js
+++ src/Utils/data.js
@@ -1,114 +1,113 @@
 export const SKILLS = [
-    {
-        title: "Frontend",
-        icon: "./img/front-end.png",
-        skills: [
-            {skill: "HTML5", percentage:"75%"},
-            {skill: "CSS3", percentage:"60%"},
-            {skill: "JavaScript", percentage:"50%"},
-            {skill: "ReactJS", percentage:"70%"},
-        ],
-    },
-    {
-        title: "Backend",
-        icon: "./img/backend.png",
-        skills: [
-            {skill: "MySql", percentage:"50%"},
-        ],
-    },
-    {
-        title: "Tools",
-        icon: "./img/tools.png",
-        skills: [
-            {skill: "Git/GitHub", percentage:"60%"},
-            {skill: "Bootstrap 5", percentage:"50%"},
-            {skill: "Visual Studio Code", percentage:"75%"},
-            {skill: "Figma", percentage:"60%"},
-            {skill: "Adobe Photoshop", percentage:"80%"},
-            
-        ],
-    },
-    {
-        title: "Soft Skills",
-        icon: "./img/softskills.png",
-        skills: [
-            {skill: "Problem Solving", percentage:"75%"},
-            {skill: "Collaboration", percentage:"50%"},
-            {skill: "Attention to detail", percentage:"80%"},
-       ],
-    },
-]
+  {
+    title: "Frontend",
+    icon: "./img/front-end.png",
+    skills: [
+      { skill: "HTML5", percentage: "75%" },
+      { skill: "CSS3", percentage: "60%" },
+      { skill: "JavaScript", percentage: "50%" },
+      { skill: "ReactJS", percentage: "70%" },
+    ],
+  },
+  {
+    title: "Backend",
+    icon: "./img/backend.png",
+    skills: [{ skill: "MySql", percentage: "50%" }],
+  },
+  {
+    title: "Tools",
+    icon: "./img/tools.png",
+    skills: [
+      { skill: "Git/GitHub", percentage: "60%" },
+      { skill: "Bootstrap 5", percentage: "50%" },
+      { skill: "Visual Studio Code", percentage: "75%" },
+      { skill: "Figma", percentage: "60%" },
+      { skill: "Adobe Photoshop", percentage: "80%" },
+    ],
+  },
+  {
+    title: "Soft Skills",
+    icon: "./img/softskills.png",
+    skills: [
+      { skill: "Problem Solving", percentage: "75%" },
+      { skill: "Collaboration", percentage: "50%" },
+      { skill: "Attention to detail", percentage: "80%" },
+    ],
+  },
+];
 
 export const WORK_EXPERIENCE = [
-    {
-        title: "Front End Intern at Code Lab Systems",
-        date: "October 2023 - November 2023",
-        responsibilites: [
-            "Developed a user-friendly job listing website using ReactJS, featuring a responsive design, searchable job listings, and a streamlined application process for job seekers.",
-        "Implemented intuitive search and filtering mechanisms, enabling job seekers to find relevant opportunities based on criteria such as location, job title, and skills.",
-        ],
-    },
-    {
-        title: "Salesforce Administrator Virtual Internship",
-        date: "October 2023 - November 2023",
-        responsibilites: [
-            "Utilized Salesforce tools and features such as Process Builder, Workflow Rules, and Formula Fields.",
-            "Gained hands-on experience in Salesforce administration, data management, and user support.",
-        ],
+  {
+    title: "Front End Intern at Code Lab Systems",
+    date: "October 2023 - November 2023",
+    responsibilites: [
+      "Developed a user-friendly job listing website using ReactJS, featuring a responsive design, searchable job listings, and a streamlined application process for job seekers.",
+      "Implemented intuitive search and filtering mechanisms, enabling job seekers to find relevant opportunities based on criteria such as location, job title, and skills.",
+    ],
+  },
+  {
+    title: "Salesforce Administrator Virtual Internship",
+    date: "October 2023 - November 2023",
+    responsibilites: [
+      "Utilized Salesforce tools and features such as Process Builder, Workflow Rules, and Formula Fields.",
+      "Gained hands-on experience in Salesforce administration, data management, and user support.",
+    ],
+  },
+  {
+    title: "Front-End Development Intern at Edunet Foundation",
+    date: "June 2024 - July 2024",
+    responsibilites: [
+      "Utilize front-end development tools and frameworks such as HTML, CSS, JavaScript, and React.",
+      "Gain hands-on experience in web development, project management, and user interface design.",
+    ],
+  },
+  {
+    title:
+      "Career Development Trainee, Pragati: Path to Future Program, Infosys SpringBoard",
+    date: "July 2024 - Present",
+    responsibilites: [
+      "Currently participating in Pragati: Path to Future, an exclusive career development program for women by Infosys Springboard. This comprehensive 12-week hybrid program focuses on Digital skills, including Python, Java, and DBMS, AI basics, Professional development, Leadership skills and Industry knowledge. Engaging in both virtual learning and in-person sessions at Infosys campuses to enhance my skills and prepare for future career opportunities in the tech industry.",
+    ],
+  },
+  {
+    title: "React Intern at Ukshathi Technologies PVT. LTD.",
+    date: "September 2024 - Present",
+    responsibilites: [
+      "Developing a responsive irrigation automation website using ReactJS, focusing on an interactive user interface.",
+      "Enhancing the website's user experience by implementing service carousels using React Slick and Bootstrap with autoplay and center mode features.",
+      "Collaborating with the development team to optimize front-end components for mobile and desktop compatibility, ensuring cross-browser performance.",
+    ],
+  },
+];
 
-    },
-    {
-        title: "Front-End Development Intern at Edunet Foundation",
-        date: "June 2024 - July 2024",
-        responsibilites: [
-            "Utilize front-end development tools and frameworks such as HTML, CSS, JavaScript, and React.",
-        "Gain hands-on experience in web development, project management, and user interface design.",
-        ],
+export const Education_details = [
+  {
+    title: "B.E in Computer Science and Business Systems",
+    name: "St Joseph Engineering College",
+    year: "2021-2025",
+    marks: "8.6 cgpa",
+  },
+  {
+    title: "Pre University (12th grade)",
+    name: "St Aloysius PU College",
+    year: "2020-2021",
+    marks: "94%",
+  },
+  {
+    title: "SSLC",
+    name: "St Agnes CBSE School",
+    year: "2018-2019",
+    marks: "93%",
+  },
+];
 
-    },
-    {
-        title: "Career Development Trainee, Pragati: Path to Future Program, Infosys SpringBoard",
-        date: "July 2024 - Ongoing",
-        responsibilites: [
-            "Currently participating in Pragati: Path to Future, an exclusive career development program for women by Infosys Springboard. This comprehensive 12-week hybrid program focuses on Digital skills, including Python, Java, and DBMS, AI basics, Professional development, Leadership skills and Industry knowledge. Engaging in both virtual learning and in-person sessions at Infosys campuses to enhance my skills and prepare for future career opportunities in the tech industry.",
-        ],
-
-    },
-
-]
-
-export const Education_details=[
-    {
-        title: "B.E in Computer Science and Business Systems",
-        name: "St Joseph Engineering College",
-        year: "2021-2025",
-        marks: "8.6 cgpa"
-    },
-    {
-        title:"Pre University (12th grade)",
-        name: "St Aloysius PU College",
-        year: "2020-2021",
-        marks: "94%"
-    },
-    {
-        title: "SSLC",
-        name: "St Agnes CBSE School",
-        year: "2018-2019",
-        marks: "93%"
-
-    }
-]
-
-export const Project_info=[
-    {
-        image: "./img/skillsphere.jpg",
-        title: "SkillSphere - Job Listing Website",
-        techstack: "ReactJS",
-        link: "https://github.com/19Sonali/SkillSphere",
-        description: "SkillSphere is a ReactJS-based job listing website designed for both job seekers and admins. Job seekers can easily browse and apply for jobs, while admins have a user-friendly interface to manage job postings. This streamlined approach ensures an efficient experience for both users and recruiters."
-        
-    },
-    
-    
-
-]
+export const Project_info = [
+  {
+    image: "./img/skillsphere.jpg",
+    title: "SkillSphere - Job Listing Website",
+    techstack: "ReactJS",
+    link: "https://github.com/19Sonali/SkillSphere",
+    description:
+      "SkillSphere is a ReactJS-based job listing website designed for both job seekers and admins. Job seekers can easily browse and apply for jobs, while admins have a user-friendly interface to manage job postings. This streamlined approach ensures an efficient experience for both users and recruiters.",
+  },
+];
